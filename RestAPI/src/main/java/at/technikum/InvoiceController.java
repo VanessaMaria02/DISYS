a94ef22bf3faaf7dd5f3c144d0ca8a33445f070c
Bojanacvetkovska <71123066+Bojanacvetkovska@users.noreply.github.com>
2023-05-24 22:41:33 +0200

    no message

--- RestAPI/src/main/java/at/technikum/InvoiceController.java
+++ RestAPI/src/main/java/at/technikum/InvoiceController.java
@@ -1,4 +1,4 @@
-package org.example;
+package at.technikum;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -25,4 +25,3 @@ public class InvoiceController {
         return "404 NOT FOUND";
     }
 }
-
